68672274326b9865c99a5bc0ee43c18d0259f924
Günce Mülayim <gnceitir96@hotmail.com>
2021-05-18 18:51:11 +0300

    kırkıncıders

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adının Boş Geçemezsiniz");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı boş geçemezsin");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girin");
+            RuleFor(x => x.CategoryName).MinimumLength(20).WithMessage("en fazla 20 karakter");
+
+        }
+    }
+}
